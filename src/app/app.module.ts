e57d91a3843579702b8bcd37a2338f7d05772a54
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-02-19 23:33:18 -0600

    NAVEGACION

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/navegacion/navegacion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navegacion',
+  templateUrl: './navegacion.component.html'
+})
+export class NavegacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
