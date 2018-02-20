857f95f679eb57da00082c240e3c071972861dc8
Samuel Ramirez <samuel_ramirez@live.com.mx>
2018-02-20 12:43:07 -0600

    FORMULARIO PARA GUARDAR PREGUNTAS DE TEXTO ABIERTO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavegacionComponent } from './components/navegacion/navegacion.component';
@@ -18,7 +19,8 @@ import { CuestionarioComponent } from './components/cuestionario/cuestionario.co
     CuestionarioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/preguntas/texto.component.ts
+++ src/app/components/preguntas/texto.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+declare var $: any;
 
 @Component({
   selector: 'app-texto',
   templateUrl: './texto.component.html'
 })
 export class TextoComponent implements OnInit {
+  preguntaTexto:FormGroup;
+  pregunta = {
+    tipo: 1,
+    pregunta: null
+  }
 
-  constructor() { }
+  constructor() {
+    this.preguntaTexto = new FormGroup({
+      'pregunta': new FormControl( '', [
+                                         Validators.minLength(3),
+                                         Validators.required
+                                        ])
+    });
+  }
 
   ngOnInit() {
+    $(document).ready(function(){
+      $('.modal').modal();
+    });
+  }
+
+  guardaTexto(){
+    this.pregunta.pregunta = this.preguntaTexto.controls['pregunta'].value;
+
+    console.log(this.pregunta);
+  }
+
+  cerrarModal(tipo){
+    $('#modal'+tipo).modal('close');
   }
 
 }
